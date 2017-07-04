c3a197e1d1b8878f0a5fbb24db376a5eeef7a8ff
sebnmuller <sebnmuller@gmail.com>
2017-07-04 15:56:44 +0200

    line 1 completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react'
-import logo from './logo.svg'
 import './App.css'
 import map from './imgs/Oslo_T-bane_linjekart.svg.png'
 
@@ -52,6 +51,19 @@ class App extends Component {
           <area id="trosterud" alt="Trosterud" onTouchEnd={this.clickThis} onClick={this.clickThis} className="rect" shape="rect" coords="1621, 647, 1666, 671" href=""/>
           <area id="haugerud" alt="Haugerud" onTouchEnd={this.clickThis} onClick={this.clickThis} className="rect" shape="rect" coords="1621, 701, 1666, 723" href=""/>
           <area id="tveita" alt="Tveita" onTouchEnd={this.clickThis} onClick={this.clickThis} className="rect" shape="rect" coords="1621, 753, 1666, 775" href=""/>
+
+          <area id="hellerud" alt="Hellerud" onTouchEnd={this.clickThis} onClick={this.clickThis} className="rect" shape="rect" coords="1548, 795, 1572, 861" href=""/>
+
+          <area id="brynseng" alt="Brynseng" onTouchEnd={this.clickThis} onClick={this.clickThis} className="rect" shape="rect" coords="1475, 795, 1499, 882" href=""/>
+          <area id="brynseng" alt="Brynseng" onTouchEnd={this.clickThis} onClick={this.clickThis} className="rect" shape="rect" coords="1401, 795, 1426, 882" href=""/>
+          <area id="ensjoe" alt="Ensjø" onTouchEnd={this.clickThis} onClick={this.clickThis} className="rect" shape="rect" coords="1328, 795, 1353, 882" href=""/>
+
+          <area id="toeyen" alt="Tøyen" onTouchEnd={this.clickThis} onClick={this.clickThis} className="poly" shape="poly" coords="1244, 954, 1239, 954, 1146, 861, 1146, 855, 1158, 843, 1163, 843, 1257, 937, 1257, 941" href=""/>
+          <area id="groennland" alt="Grønnland" onTouchEnd={this.clickThis} onClick={this.clickThis} className="poly" shape="poly" coords="1186, 1013, 1184, 1013, 1089, 919, 1089, 914, 1102, 901, 1106, 901, 1200, 995, 1200, 1000" href=""/>
+          <area id="jernbanetorget" alt="Jernbanetorget" onTouchEnd={this.clickThis} onClick={this.clickThis} className="poly" shape="poly" coords="1038, 1071, 1035, 1068, 1035, 945, 1038, 942, 1056, 942, 1059, 945, 1059, 1068, 1056, 1071" href=""/>
+          <area id="stortinget" alt="Stortinget" onTouchEnd={this.clickThis} onClick={this.clickThis} className="poly" shape="poly" coords="973, 1071, 976, 1066, 976, 947, 973, 942, 954, 942, 951, 945, 951, 1068, 954, 1071" href=""/>
+          <area id="nationaltheatret" alt="Nationaltheatret" onTouchEnd={this.clickThis} onClick={this.clickThis} className="poly" shape="poly" coords="870, 1071, 867, 1067, 867, 946, 870, 942, 889, 942, 892, 945, 892, 1068, 889, 1071" href=""/>
+          <area id="majorstuen" alt="Majorstuen" onTouchEnd={this.clickThis} onClick={this.clickThis} className="poly" shape="poly" coords="695, 968, 695, 964, 789, 870, 794, 870, 807, 884, 807, 886, 713, 981, 708, 981" href=""/>
 
         </map>
 
