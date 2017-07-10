7f29699a8b89fac9e49f76eb4bf1f9402e0773d8
sebnmuller <sebnmuller@gmail.com>
2017-07-10 12:57:31 +0200

    fetch works

--- src/App.js
+++ src/App.js
@@ -11,14 +11,43 @@ class App extends Component {
     e.target.style.fill = "red"
     document.e.target.id.focus()
 
+  }
 
+  loadData(url) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(response, e) {
+
+      console.log(response)
+
+      if (xhttp.readyState === 4 && xhttp.status === 200) {
+        console.log(xhttp.responseText);
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }
+
+  getInfo(url) {
+    console.log('wat')
+    return fetch(url).then((response) => response.json()).then((responseJson) => {
+
+console.log(url)
+console.log(responseJson)
+
+
+
+      return responseJson.movies;
+    }).catch((error) => {
+      console.error(error);
+    });
   }
 
   render() {
+
+    this.getInfo('http://reisapi.ruter.no/Line/GetLinesRuterExtended?ruterOperatedOnly=true')
+
     return (
-      <div className="App">
-      
-      </div>
+      <div onClick={this.test} className="App"></div>
     )
   }
 }
