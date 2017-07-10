83fc880c8ab733e5939872d01268e64945d28243
sebnmuller <sebnmuller@gmail.com>
2017-07-10 16:43:53 +0200

    logging all stops

--- src/App.js
+++ src/App.js
@@ -3,23 +3,7 @@ import './App.css'
 
 class App extends Component {
 
-  contructor(props) {
-
-    this.state = {
-      stateStops: []
-    }
-  }
-
-  componentDidMount() {
-    var self = this
-    var stops = this.getInfo('http://reisapi.ruter.no/Line/GetLinesRuterExtended?ruterOperatedOnly=true')
-
-    self.setState({stateStops: stops})
-
-
-  }
-
-  getInfo(url) {
+  getData(url) {
 
     return fetch(url).then((response) => response.json()).then((responseJson) => {
       return responseJson.filter(function(line) {
@@ -32,7 +16,15 @@ class App extends Component {
 
   render() {
 
-    var stops = this.getInfo('http://reisapi.ruter.no/Line/GetLinesRuterExtended?ruterOperatedOnly=true')
+    var lines = this.getData('http://reisapi.ruter.no/Line/GetLinesRuterExtended?ruterOperatedOnly=true')
+
+    var test = lines.then(function(result) {
+      result.map((line) => {
+        return line.map((stop) => {
+          console.log(stop.Name.replace('[T-bane]', '').trim())
+        })
+      })
+    })
 
     return (
 
