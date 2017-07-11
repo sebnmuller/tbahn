79b20ca93f7b7e076e43d0b2ddd21015e1e6cf97
sebnmuller <sebnmuller@gmail.com>
2017-07-11 09:39:04 +0200

    inlined a lot of stuff

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ class App extends Component {
 
   getData(url) {
 
-    return fetch(url).then((response) => response.json()).then((responseJson) => {
-      return responseJson.filter(function(line) {
-        return line.Transportation === 8
-      }).map((tbahnLine) => {
-        return tbahnLine.Stops;
-      })
-    })
+    return fetch(url).then(response => response.json()).then(responseJson =>
+       responseJson.filter(({Transportation}) => Transportation === 8)
+        .map(({Stops}) => Stops)
+    )
   }
 
   render() {
@@ -19,9 +16,9 @@ class App extends Component {
     var lines = this.getData('http://reisapi.ruter.no/Line/GetLinesRuterExtended?ruterOperatedOnly=true')
 
     var test = lines.then(function(result) {
-      result.map((line) => {
-        return line.map((stop) => {
-          console.log(stop.Name.replace('[T-bane]', '').trim())
+      result.map(line => {
+        return line.map(({Name}) => {
+          console.log(Name.replace('[T-bane]', '').trim())
         })
       })
     })
